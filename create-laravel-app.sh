51f4b8d99f73b2a6c44da3179747fffaca85e495
sebashack <sebastian0092@gmail.com>
2023-02-14 14:15:41 -0500

    Switch to laravel 10.X

--- create-laravel-app.sh
+++ create-laravel-app.sh
@@ -4,4 +4,4 @@ export INSTALL_DIR="$ROOT/_install"
 export COMPOSER_HOME="$INSTALL_DIR/composer"
 export COMPOSER_BIN="php $INSTALL_DIR/composer/composer.phar"
 
-$COMPOSER_BIN create-project --prefer-dist laravel/laravel src "9.*"
+$COMPOSER_BIN create-project --prefer-dist laravel/laravel src "10.*"
