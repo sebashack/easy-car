7982a450d8916681e17eab9c2e43448e1fd9398f
sebashack <sebastian0092@gmail.com>
2023-02-25 10:11:20 -0500

    Add temporary links to home view which lead to review operations + update layout title dynamically

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 @section('content')
 <div class="text-center">
-  Welcome to EasyCar
+  <h1>Welcome to EasyCar</h1>
+  </br>
+
+  <div>
+    <a href="">View car model reviews</a>
+  </div>
+  <div>
+    <a href="">Create a review for a car model</a>
+  </div>
 </div>
 @endsection
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
-  <title>EasyCar</title>
+  <title>@yield('title', 'EasyCar')</title>
 </head>
 <body>
   <!-- header -->
