8b2c69bb46ca3d06765bbae5d85c3fd09b16b09b
sebashack <sebastian0092@gmail.com>
2023-02-25 10:01:04 -0500

    Implement minimalist layout

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>EasyCar</title>
-    </head>
-    <body class="antialiased">
-        <div>
-            <h1>EasyCar</h1>
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+<div class="text-center">
+  Welcome to EasyCar
+</div>
+@endsection
