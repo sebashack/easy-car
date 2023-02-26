8ddb1de53547b584615ead74662b3e75510a7a69
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-02-25 19:03:00 -0500

    Create the view to add a car model

---
+++ src/resources/views/carModel/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section("title", $viewData["title"])
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add Car Model</div>
+          <div class="card-body">
+            <form method="POST" action="{{ route('carModel.save') }}">
+              @csrf
+              <input type="text" class="form-control mb-2" placeholder="Enter brand" name="brand" value="{{ old('brand') }}" />
+              <input type="text" class="form-control mb-2" placeholder="Enter model" name="model" value="{{ old('model') }}" />
+              <inout type="text" class="form-control mb-2" placeholder="Enter description" name="description" value="{{ old('description') }}" />
+              <input type="submit" class="btn btn-primary" value="Send" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+@endsection
