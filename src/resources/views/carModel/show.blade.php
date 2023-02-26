c32b1eaf9569f724d82b49e39c8fb2673624c2d9
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-02-25 20:51:30 -0500

    Add button to delete the specific object

--- src/resources/views/carModel/show.blade.php
+++ src/resources/views/carModel/show.blade.php
@@ -11,6 +11,12 @@
         <p class="card-text">Brand: {{ $viewData['carModel']['brand'] }}</p>
         <p class="card-text">Model: {{ $viewData['carModel']['model'] }}</p>
         <p class="card-text">Description: {{ $viewData['carModel']['description'] }}</p>
+
+        <form action="{{ route('carModel.delete', ['id'=> $viewData['carModel']['id']]) }}" method="post">
+          <input class="btn bg-primary text-white" type="submit" value="Delete" />
+          @csrf
+          @method('delete')
+        </form 
       </div>
     </div>
   </div>
