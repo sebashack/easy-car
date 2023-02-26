fa8d9e2b68e4beab4fc2c986b1f73d56d30aede4
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-02-25 20:15:15 -0500

    Implemet the view to show the car models in the database

--- src/resources/views/carModel/index.blade.php
+++ src/resources/views/carModel/index.blade.php
@@ -4,5 +4,18 @@
 <div class="text-center">
   <h1>Car Models</h1>
   </br>
+  </br>
+  </br>
+  @foreach ($viewData["carModels"] as $carModel)
+    <div class="col-md-4 col-lg-3 mb-2">
+    <div class="card">
+      <div class="card-body text-center">
+        <p>Brand: {{ $carModel["brand"] }}</p>
+        <p>Model: {{ $carModel["model"] }}</p>
+        <p>Description: {{ $carModel["description"] }}</p>
+      </div>
+    </div>
+  </div>
+  @endforeach
 </div>
 @endsection
