b636b239c83d81b9e9e1e1f41209f7e38c4a8da9
sebashack <sebastian0092@gmail.com>
2023-03-05 17:27:07 -0500

    Include script to manage local composer installation

---
+++ composer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ROOT="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )" )"
+export INSTALL_DIR="$ROOT/_install"
+export COMPOSER_HOME="$INSTALL_DIR/composer"
+export COMPOSER_BIN="php $INSTALL_DIR/composer/composer.phar"
+
+cd $ROOT/src
+$COMPOSER_BIN "$@"
