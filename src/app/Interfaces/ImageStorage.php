84379be3c04abcba49bc304eb7a84e16c92f38cb
sochoac1 <sochoac1@eafit.edu.co>
2023-03-06 17:47:07 -0500

    dependency inversion and dependency injection to upload images

---
+++ src/app/Interfaces/ImageStorage.php
@@ -0,0 +1,8 @@
+<?php 
+
+namespace App\Interfaces;
+use Illuminate\Http\Request;
+
+interface ImageStorage {
+    public function store(Request $request): string;
+}
---
+++ src/app/Util/ImageLocalStorage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Util;
+
+use App\Interfaces\ImageStorage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ImageLocalStorage implements ImageStorage
+{
+    public function store(Request $request): string
+    {
+        $newImageName = 'images/' . uniqid() . '-' . 'car' . '.' . $request->image_uri->extension();
+        if ($request->hasFile('image_uri')) {
+            Storage::disk('public')->put(
+                $newImageName,
+                file_get_contents($request->file('image_uri')->getRealPath())
+            );
+        }
+        return storage_path('app/public/') . $newImageName;
+    }
+}
