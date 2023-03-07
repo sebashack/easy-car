79eaeb38aae2ac509fff6a811a8b2eb18855ee0b
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-07 18:33:48 -0500

    Implement relations BelongsTo inside the Review model

--- src/app/Models/Review.php
+++ src/app/Models/Review.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use App\Models\CarModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -12,8 +14,30 @@ class Review extends Model
      * $this->attributes['id'] - int - contains the review primary key (id)
      * $this->attributes['content'] - string - contains the review's text content
      * $this->attributes['rating'] - int - contains the car model's numeric rating (from 1 to 5)
+     * $this->attributes['car_model_id'] - int - contains the foreign key of the corresponding car model
+     * $this->carModel - CarModel - contains the associated car model
      */
-    protected $fillable = ['content', 'rating'];
+    protected $fillable = ['content', 'rating','car_model_id'];
+
+    public function carModel(): BelongsTo
+    {
+        return $this->belongsTo(CarModel::class);
+    }
+
+    public function setCarModel(CarModel $carModel): void 
+    {
+        $this->carModel = $carModel;
+    }
+
+    public function getCarModel(): CarModel 
+    {
+        return $this->carModel;
+    }
+
+    public function getCarModelId(): int 
+    {
+        return $this->attribues['car_model_id'];
+    }
 
     public function getId(): int
     {
