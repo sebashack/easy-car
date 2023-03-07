edcdf3cd5526eef3977033ccc2f0b9226b676851
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-07 17:39:35 -0500

    Implement migration that relates car-models to car

---
+++ src/database/migrations/2023_03_07_223243_add_foreign_key_to_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->unsignedBigInteger('car_model_id');
+            $table->foreign('car_model_id')
+                   ->references('id')
+                   ->on('car_models')
+                   ->onDelete('cascade')
+                   ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropColumn('car_model_id');
+        });
+    }
+};
