355df53ddee6a396b77f00a8fe356238e88e79b3
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-07 18:58:39 -0500

    Implement relation between User and Review models

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Review;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -42,6 +44,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relation with Review
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function getReviews(): Collection
+    {
+        return $this->reviews;
+    }
+
+    public function setReviews(Collection $reviews): void 
+    {
+        $this->reviews = $reviews;
+    }
 
     public function getId(): int
     {
