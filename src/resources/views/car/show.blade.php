ece533e3cc89735b7511540f19912c08e3297488
sebashack <sebastian0092@gmail.com>
2023-03-09 09:11:58 -0500

    Fix typo + remove trailing white spaces

--- src/resources/views/car/show.blade.php
+++ src/resources/views/car/show.blade.php
@@ -9,27 +9,26 @@
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">
-           ID: {{ $viewData['car']->getId() }} 
+           ID: {{ $viewData['car']->getId() }}
         </h5>
         <p>{{ __('Color') }}: {{ $viewData['car']->getColor() }}</p>
         <p>{{ __('Price') }}: ${{ $viewData['car']->getPrice() }}</p>
         <p>{{ __('Kilometers') }}: {{ $viewData['car']->getKilometers() }} km </p>
 
         <p>
-        @if ($viewData['car']->getIsNew()) 
+        @if ($viewData['car']->getIsNew())
           {{ __('New Model') }}
-        @else 
+        @else
           {{ __('Used Car') }}
-        @endif   
+        @endif
         </p>
         <p>
-        @if ($viewData['car']->getIsAvailable()) 
+        @if ($viewData['car']->getIsAvailable())
           {{ __('Available') }}
-        @else 
-          {{ __('No Available') }}
-        @endif   
+        @else
+          {{ __('Not Available') }}
+        @endif
         </p>
-
 
         <form action="{{ route('car.delete', ['id'=> $viewData['car']->getId()]) }}" method="post">
           <input class="btn bg-primary text-white" type="submit" value="{{ __('Delete') }}" />
