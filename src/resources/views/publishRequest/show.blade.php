037de8ece27a07d9b197543fad4f7fbcec55c7cd
ITAKEB <tomascf123@gmail.com>
2023-03-12 18:49:35 -0500

    Add lang for publish-requests

--- src/resources/views/publishRequest/show.blade.php
+++ src/resources/views/publishRequest/show.blade.php
@@ -10,13 +10,13 @@
            Id: {{ $viewData['publishRequest']->getId() }}
         </h5>
         <h5 class="card-title">
-           State: {{ $viewData['publishRequest']->getState() }}
+           {{__('State')}}: {{ $viewData['publishRequest']->getState() }}
         </h5>
         <h5 class="card-title">
-           Message: {{ $viewData['publishRequest']->getMessage() }}
+           {{('Message')}}: {{ $viewData['publishRequest']->getMessage() }}
         </h5>
         <form action="{{ route('publishRequest.delete', ['id'=> $viewData['publishRequest']->getId()]) }}" method="post">
-          <input class="btn bg-primary text-white" type="submit" value="Delete" />
+          <input class="btn bg-primary text-white" type="submit" value="{{__('Delete')}}" />
           @csrf
           @method('delete')
         </form  >
