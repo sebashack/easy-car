3da23a31914b4b08a8c4bb89ef0b090d4520bfc0
sochoac1 <sochoac1@eafit.edu.co>
2023-03-12 15:21:07 -0500

    Deleting duplicated yield in layout

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -54,6 +54,5 @@
   @yield('scripts')
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
   </script>
-  @yield('scripts')
 </body>
 </html>
