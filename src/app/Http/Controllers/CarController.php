87d884d7eb3bfe3652c02dd8490e8de6a091810b
sochoac1 <sochoac1@eafit.edu.co>
2023-03-12 15:55:38 -0500

    Adding car_model_id to create view

--- src/app/Http/Controllers/CarController.php
+++ src/app/Http/Controllers/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Interfaces\ImageStorage;
 use App\Models\Car;
+use App\Models\CarModel;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -29,7 +30,7 @@ class CarController extends Controller
     {
         $viewData = [];
         $viewData['title'] = 'Create Car';
-
+        $viewData['carModels'] = CarModel::all();
         return view('car.create')->with('viewData', $viewData);
     }
 
