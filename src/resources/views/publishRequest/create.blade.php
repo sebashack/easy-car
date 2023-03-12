93fdf4f3f55ae4e706aca0f70465ba0b0a4b4f9e
ITAKEB <tomascf123@gmail.com>
2023-03-12 17:02:50 -0500

    Extend publish-request form

--- src/resources/views/publishRequest/create.blade.php
+++ src/resources/views/publishRequest/create.blade.php
@@ -13,42 +13,51 @@
                         <li>{{ $error }}</li>
                         @endforeach
                     </ul>
-                    @endif @if (session('status'))
+                    @endif
+                    @if (session('status'))
                     <div class="alert alert-success">
-                        {{ session("status") }}
+                        {{ session('status') }}
                     </div>
                     @endif
-                    <form
-                        method="POST"
-                        action="{{ route('publishRequest.save') }}"
-                    >
+                    <form method="POST" action="{{ route('publishRequest.save') }}" enctype="multipart/form-data">
                         @csrf
-                        <input
-                            type="text"
-                            class="form-control mb-2"
-                            placeholder="Enter message"
-                            name="message"
-                            value="{{ old('message') }}"
-                        />
-                        <select
-                            class="form-select mb-2"
-                            name="state"
-                            value="{{ old('state') }}"
-                        >
-                            <option selected>Choose an option</option>
-                            <option value="pending">Pending</option>
-                            <option value="accepted">Accepted</option>
-                            <option value="rejected">Rejected</option>
-                        </select>
-                        <input
-                            type="submit"
-                            class="btn btn-primary"
-                            value="Send"
-                        />
+                        <input type="text" class="form-control mb-2" placeholder="Enter message for admin" name="message" value="{{ old('message') }}" />
+                        <h6>Car info</h6>
+                        <input type="text" class="form-control mb-2" placeholder="Enter car color" name="color" value="{{ old('color') }}" />
+                        <input type="number" class="form-control mb-2" placeholder="{{ __('Enter kilometers') }}" name="kilometers" value="{{ old('kilometers') }}" />
+                        <input type="number" class="form-control mb-2" placeholder="{{ __('Enter price') }}" name="price" value="{{ old('price') }}" />
+                        <div class="form-group">
+                            <label>{{ __('Image') }}:</label>
+                            <input type="file" name="image_uri" />
+                        </div>
+                        <div class="form-group">
+                            <label for="transmission">{{ __('Transmission') }}:</label>
+                            <select class="form-select mb-2" name="transmission_type">
+                                <option value="automatic">{{ __('Automatic') }}</option>
+                                <option value="mechanic">{{ __('Mechanic') }}</option>
+                                <option value="triptronic">{{ __('Triptonic') }}</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="vehicle-type">{{ __('Type of Vehicle') }}:</label>
+                            <select class="form-select mb-2" name="type">
+                                <option value="van">{{ __('Van') }}</option>
+                                <option value="sedan">{{ __('Sedan') }}</option>
+                                <option value="truck">{{ __('Truck') }}</option>
+                                <option value="suv">{{ __('SUV') }}</option>
+                                <option value="coupe">{{ __('Coupe') }}</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="year">{{ __('Manufacture year') }}:</label>
+                            <input type="number" class="form-control" name="manufacture_year" min="2000" max="2023">
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="{{ __('Send') }}" />
                     </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
