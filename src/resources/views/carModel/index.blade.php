bb56c10d9c60076d17efea2410c4c368432e9d63
sochoac1 <sochoac1@eafit.edu.co>
2023-03-20 17:25:53 -0500

    Center carmodels index

--- src/resources/views/carModel/index.blade.php
+++ src/resources/views/carModel/index.blade.php
@@ -1,36 +1,38 @@
 @extends('layouts.app')
 @section('title', $viewData["title"])
 @section('content')
-<div class="text-center">
-    <div>
+<div class="text-left">
+    <div class="text-center">
         <h1>{{ __("Car Models") }}</h1>
     </div>
-    @foreach ($viewData["carModels"] as $carModel)
-    <div class="col-md-4 col-lg-3 mb-2">
-        <div class="card">
-            <div class="card-body text-center">
-                <p>{{ __("Brand") }}: {{ $carModel->getBrand() }}</p>
-                <p>{{ __("Model") }}: {{ $carModel->getModel() }}</p>
-                <p>
-                    {{ __("Description") }}: {{ $carModel->getDescription() }}
-                </p>
-                <a
-                    href="{{ route('carModel.show', ['id'=> $carModel->getId()]) }}"
-                    class="btn bg-primary text-white"
-                >
-                    {{ __("Check car model") }}
-                </a>
-                @if ($viewData['is_admin'])
+    <div class="row">
+        @foreach ($viewData["carModels"] as $carModel)
+        <div class="col-md-4 col-lg-3 mb-2">
+            <div class="card">
+                <div class="card-body text-center">
+                    <p>{{ __("Brand") }}: {{ $carModel->getBrand() }}</p>
+                    <p>{{ __("Model") }}: {{ $carModel->getModel() }}</p>
+                    <p>
+                        {{ __("Description") }}: {{ $carModel->getDescription() }}
+                    </p>
                     <a
-                        href="{{ route('carModel.edit', ['id'=> $carModel->getId()]) }}"
+                        href="{{ route('carModel.show', ['id'=> $carModel->getId()]) }}"
                         class="btn bg-primary text-white"
                     >
-                    <span>&#9998;</span>
+                        {{ __("Check car model") }}
                     </a>
-                @endif
+                    @if ($viewData['is_admin'])
+                        <a
+                            href="{{ route('carModel.edit', ['id'=> $carModel->getId()]) }}"
+                            class="btn bg-primary text-white"
+                        >
+                        <span>&#9998;</span>
+                        </a>
+                    @endif
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
