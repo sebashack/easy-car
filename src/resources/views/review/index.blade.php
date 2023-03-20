726f1f6698eec8abe8334a6843b725e06f9d8c6e
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-20 15:16:50 -0500

    Bug´s fixed

--- src/resources/views/review/index.blade.php
+++ src/resources/views/review/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="text-center">
     <h1>{{ __("Reviews") }}</h1>
-    <br />
     <div class="row">
         @foreach ($viewData["reviews"] as $review)
         <div class="col-md-4 col-lg-3 mb-2">
