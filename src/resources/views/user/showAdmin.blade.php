ffc34beaa8d707a34eb9268a5b588e62013f7944
sebashack <sebastian0092@gmail.com>
2023-03-20 16:57:37 -0500

    Add some links to admin profile

--- src/resources/views/user/showAdmin.blade.php
+++ src/resources/views/user/showAdmin.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 @section('title', $viewData["title"])
 @section('content')
-<div class="card mb-3">
-    <div class="row g-0">
-        <div class="col-md-8">
-            <div class="card-body">
-                <h3 class="card-title">{{ $viewData['user']->getName() }} {{ $viewData['user']->getLastName() }}</h3>
-                <a href="{{ route('carModel.create') }}" class="link-info"> Create car model </a>
-            </div>
-        </div>
+<h1 class="card-title">{{ $viewData['user']->getName() }} {{ $viewData['user']->getLastName() }}</h1>
+<br/>
+<br/>
+<div>
+    <div>
+        <h3 class="card-title">Car models</h3>
+        <ul class="list-group">
+            <a href="{{ route('carModel.index') }}" class="list-group-item link-info"> View car models </a>
+            <a href="{{ route('carModel.create') }}" class="list-group-item link-info"> Register new car model </a>
+        </ul>
     </div>
-</div>
+    <br/>
+    <div>
+        <h3 class="card-title">Cars</h3>
+        <ul class="list-group">
+            <a href="{{ route('car.index') }}" class="list-group-item link-info"> View cars </a>
+            <a href="{{ route('car.create') }}" class="list-group-item link-info"> Register new car </a>
+        </ul>
+    </div>
+<div>
 @endsection
