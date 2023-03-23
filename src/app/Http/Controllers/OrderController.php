bb358106b0c1ffe7190d0a25b306dfa7ecbd02d5
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-23 18:36:26 -0500

    Change pdf method name

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -120,7 +120,7 @@ class OrderController extends Controller
         return back();
     }
 
-    public function pdf(string $id)
+    public function download_pdf(string $id)
     {
         $order = Order::findOrFail($id);
         $pdf_name = 'invoice-of-order-'.$order->getId().'.pdf';
