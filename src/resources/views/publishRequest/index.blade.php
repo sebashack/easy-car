1c9809fc63d88c11bbbee4bb75c665a71dc2bddd
sebashack <sebastian0092@gmail.com>
2023-03-28 20:16:49 -0500

    Fix wrapping of items in publish requests

--- src/resources/views/publishRequest/index.blade.php
+++ src/resources/views/publishRequest/index.blade.php
@@ -4,17 +4,19 @@
 <div class="text-center">
   <h1>{{__('Car publish requests')}}</h1>
   </br>
-  @foreach ($viewData["publishRequests"] as $publishRequest)
+  <div class="row">
+    @foreach ($viewData["publishRequests"] as $publishRequest)
     <div class="col-md-4 col-lg-3 mb-2">
-    <div class="card">
-      <div class="card-body text-center">
-        <p><strong>{{__('Id') }}:</strong> {{ $publishRequest->getId() }}</p>
-        <p><strong>{{__('User') }}:</strong> {{ $publishRequest->getUser()->getName() }} {{ $publishRequest->getUser()->getLastName() }}</p>
-        <p><strong>{{__('State') }}:</strong> {{ __($publishRequest->getState()) }}</p>
-        <a href="{{ route('publishRequest.show', ['id'=> $publishRequest->getId()]) }}" class="btn action-bg-color">{{__('See More')}}</a>
+      <div class="card">
+        <div class="card-body text-center">
+          <p><strong>{{__('Id') }}:</strong> {{ $publishRequest->getId() }}</p>
+          <p><strong>{{__('User') }}:</strong> {{ $publishRequest->getUser()->getName() }} {{ $publishRequest->getUser()->getLastName() }}</p>
+          <p><strong>{{__('State') }}:</strong> {{ __($publishRequest->getState()) }}</p>
+          <a href="{{ route('publishRequest.show', ['id'=> $publishRequest->getId()]) }}" class="btn action-bg-color">{{__('See More')}}</a>
+        </div>
       </div>
     </div>
+    @endforeach
   </div>
-  @endforeach
 </div>
 @endsection
